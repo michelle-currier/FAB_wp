33b8cc898478879fee0ceb298c9f2620cf8c25b7
Michelle-currier <mushel@gmail.com>
2024-07-01 10:06:56 -0500

    updates

--- wp-content/themes/fabulous/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-function fabulous_enqueue_styles() {
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400&family=Super+Sunday&display=swap', false);
-}
-add_action('wp_enqueue_scripts', 'fabulous_enqueue_styles');
-?>
